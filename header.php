320fde48478df234e72a7dbc992f0ddee9d9eed6
Adrian Gracia Aranda <adriangracia9@gmail.com>
2023-07-09 16:07:20 -0400

    Updated Google Analytics tag

--- header.php
+++ header.php
@@ -2,6 +2,18 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-3E37T94658"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-3E37T94658');
+  </script>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
